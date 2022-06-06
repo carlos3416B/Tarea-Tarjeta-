0860459e41229f227d5f916844cd9bdd0ac5f6a1
carlos3416B <105512546+carlos3416B@users.noreply.github.com>
2022-06-06 01:58:05 -0500

    Tarea hecha

---
+++ fichatecnica.js
@@ -0,0 +1,5 @@
+document.write("<section>")
+
+document.write("<table><tr><td><img src=https://c-fa.cdn.smule.com/rs-z-sf-1/account/picture/76/be/674f2e2c-9507-41be-a5aa-e824a4622353.jpg></td><td >Nombre:<br><br>localidad:<br><br>Edad:<br><br>Profecion:</td> <td id=td1>Olivares Sotelo Carlos Daniel<br><br>Jiutepec Morelos<br><br>18<br><br>Redes</td></tr></table>");
+
+document.write("</section>")
